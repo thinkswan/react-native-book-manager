915191bbfaa1cea03972f58ad6b795e9a1fd7dc7
Graham Swan <thinkswan@gmail.com>
2022-11-04 14:53:20 -0500

    feat: list books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,40 @@
-import { SafeAreaView, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
 
-const Books = () => {
+const Books = ({ books }) => {
   return (
-    <SafeAreaView>
-      <Text>Books</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Books</Text>
+
+      <ScrollView
+        keyboardShouldPersistTaps="always"
+        style={styles.booksContainer}>
+        {books.map((book, i) => (
+          <View key={i} style={styles.book}>
+            <Text style={styles.name}>{book.name}</Text>
+            <Text style={styles.author}>{book.author}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default Books;
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    paddingTop: 30,
+    paddingBottom: 20,
+  },
+  booksContainer: { flex: 1, borderTopWidth: 1, borderTopColor: '#ddd' },
+  book: { padding: 20 },
+  name: { fontSize: 18 },
+  author: { fontSize: 14, color: '#999' },
+});
+
+const mapStateToProps = state => ({ books: state.bookReducer.books });
+
+export default connect(mapStateToProps)(Books);
