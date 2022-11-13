75602fa9988b92cf6ffb244a911ea8798d80204d
Graham Swan <thinkswan@gmail.com>
2022-11-12 23:57:22 -0600

    chore: add tab icons

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Provider } from 'react-redux';
 import About from './src/features/about/About';
 import Books from './src/features/books/Books';
 import booksReducer from './src/features/books/books-slice';
+
+Ionicons.loadFont().then();
 
 const store = configureStore({
   reducer: {
@@ -19,7 +22,34 @@ const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              if (route.name === 'Books') {
+                return (
+                  <Ionicons
+                    name={focused ? 'ios-book' : 'ios-book-outline'}
+                    size={size}
+                    color={color}
+                  />
+                );
+              }
+
+              return (
+                <Ionicons
+                  name={
+                    focused
+                      ? 'ios-information-circle'
+                      : 'ios-information-circle-outline'
+                  }
+                  size={size}
+                  color={color}
+                />
+              );
+            },
+            tabBarActiveTintColor: 'dodgerblue',
+            tabBarInactiveTintColor: 'gray',
+          })}>
           <Tab.Screen name="Books" component={Books} />
           <Tab.Screen name="About" component={About} />
         </Tab.Navigator>
